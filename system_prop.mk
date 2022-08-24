7172088533d189cd0f9323b0ec422443fa141d85
Peter Cai <peter@typeblog.net>
2022-08-24 15:47:06 -0400

    RMX3242: Initial implementation of sysbta, a system-side bluetooth audio HAL
    On platform release T, the legacy `audio.a2dp.default` HAL no longer
    exists, and cannot be trivially restored for generic system-side A2DP
    audio support. Rather than trying to cling on to its existence (which
    does not even work anymore even if one did so), it is time to come up
    with a new solution.
    
    This commit introduces a system-side implementation of the generic
    bluetooth audio HAL. The HAL itself is modelled after the default
    bluetooth audio HAL, while substantial components of the default audio
    HAL are also included as it has to also implement the audio HAL
    interfaces.
    
    The audio HAL implementation is delegated to `audio.sysbta.default`,
    forked from `audio.bluetooth.default` from the Bluetooth apex package.
    It then communicates with the bluetooth audio HAL interfaces, which need
    to live in the same process (in this case, the process of the sysbta
    HAL). This is why we cannot just load `audio.sysbta.default` into the
    default audio HAL or the audioserver process directly, but rather have
    to include our own simplistic audio HAL implementation.
    
    For now, the audio HAL implementation only includes one for the 6.0
    version, but technically, an interface for *every* audio HAL version
    needs to exist. This, along with other messiness in the sysbta
    implementation, will be addressed in a future commit.
    
    Note that to actually make use of the sysbta audio hal, patches in
    frameworks/av and packages/modules/Bluetooth are required to introduce
    support for the property `persist.bluetooth.system_audio_hal.enabled`.
    
    gsi: sysbta: Separate audio interfaces from bluetooth
    
    This will allow us to dynamically load multiple different versions of
    audio interface implementation into the same sysbta hal process. Note
    that since we are using a weird suffix here, the default
    `registerPassthroughServiceImplementation` will not work, and we will
    have to define our own dynamic symbol for it.
    
    gsi: sysbta: Implement all existing audio HAL versions
    
    This is needed because libaudiohal.so can only interact with one HAL
    version at a time (loading only one of the libaudiohal@version.so
    implementations), so only the interface that matches the stock HAL
    version will be used. libaudiohal decides which version to use by
    looking through the interfaces whose variant is `default` and picking
    the highest versioned one, so our hal will not interfere in the process
    (because we use `sysbta` as our variant).
    
    Change-Id: I0c3aa291a31092993a2e004b594384630abd8183
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -7,3 +7,31 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Privapp permissions whitelisting
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=log
+
+# Audio
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.camera.sound.forced=0 \
+    ro.audio.silent=0 \
+    ro.config.vc_call_vol_steps=9
+
+# Bluetooth
+PRODUCT_PRODUCT_PROPERTIES += \
+    bluetooth.device.class_of_device?=90,2,12 \
+    bluetooth.hardware.power.operating_voltage_mv=3300 \
+    bluetooth.profile.asha.central.enabled?=true \
+    bluetooth.profile.avrcp.target.enabled?=true \
+    bluetooth.profile.a2dp.source.enabled?=true \
+    bluetooth.profile.bas.client.enabled?=true \
+    bluetooth.profile.gatt.enabled?=true \
+    bluetooth.profile.hfp.ag.enabled?=true \
+    bluetooth.profile.hid.device.enabled?=true \
+    bluetooth.profile.hid.host.enabled?=true \
+    bluetooth.profile.map.server.enabled?=true \
+    bluetooth.profile.opp.enabled?=true \
+    bluetooth.profile.pan.nap.enabled?=true \
+    bluetooth.profile.pan.panu.enabled?=true \
+    bluetooth.profile.pbap.server.enabled?=true \
+    bluetooth.profile.sap.server.enabled?=true
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.bluetooth.system_audio_hal.enabled=1

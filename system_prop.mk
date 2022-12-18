86e034adb9afda2944221a2a7cfe18a95e64ff4f
techyminati <sinha.aryan03@gmail.com>
2022-12-18 18:06:41 +0530

    RMX3242: Allow privapp permission whitelisting
    Change-Id: Ia8aa40a9be1a09276a50e75f4274e4c93d3e700c
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -3,3 +3,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
+
+# Privapp permissions whitelisting
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.control_privapp_permissions=log

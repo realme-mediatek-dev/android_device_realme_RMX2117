9add2ca3cfecbf208ac58e29f11da62c3f6319e9
techyminati <sinha.aryan03@gmail.com>
2022-12-18 18:01:19 +0530

    RMX3242: Import mtk-specific props from stock
    Change-Id: If72abab8eea96f1fc0ae30a9731825307407f3e7
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- device.mk
+++ device.mk
@@ -64,6 +64,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/etc/fstab.mt6833:$(TARGET_COPY_OUT_RAMDISK)/fstab.mt6833
 
+# System prop
+-include $(DEVICE_PATH)/system_prop.mk
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml

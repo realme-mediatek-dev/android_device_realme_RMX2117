580a5fc649cab58761a7a29df1e904395daf2149
Tim Zimmermann <tim@linux4.de>
2023-02-18 10:26:47 +0530

    RMX3242: Inherit non_ab_device.mk * This is required to build applypatch for updating recovery
    Change-Id: Ie6d3814acfaca3e149819b592f1a93ff9eb14365
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- cipher_RMX3242.mk
+++ cipher_RMX3242.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from RMX3242 device
 $(call inherit-product, device/realme/RMX3242/device.mk)
